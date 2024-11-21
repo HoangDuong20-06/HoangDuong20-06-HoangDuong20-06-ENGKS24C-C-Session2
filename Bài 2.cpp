487667a49c37cf3a66fc9ad439fef2afb95afd9d
HoangDuong20-06 <HoangDuong2062006@gmail.com>
2024-11-21 09:34:01 +0700

    Add files via upload

---
+++ Bài 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    //Khai bao va khoi tao hai so nguyen
+	int number1 = 24; // Bien number1 co gia tri bang 20
+	int number2 = 6; // Bien number2 co gia tri bang 6
+	// Khai bao va khoi tao mot bien de luu ket qua
+	int sum;
+	// Khai bao va khoi tao cac phep tinh cua hai so nguyen
+	sum = number1 + number2; // Tong cua hai so nguyen
+	// In ra ket qua
+	printf("Tong cua %d + %d = %d\n", number1, number2, sum);
+	return 0;	
+}
